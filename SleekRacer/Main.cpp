36ee37c9d2ed2af41ae3b285b098a51ba06f4d6b
Moeez Abdul <MoeezRajput2000@Gmail.com>
2023-04-04 14:16:03 +0100

    First-Commit

--- SleekRacer/Main.cpp
+++ SleekRacer/Main.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+class SleekRacer {
+
+};
+class PowerUp {
+
+};
+
+class SpeedBoost : public PowerUp {
+
+};
+
+class Obstacle {
+
+};
+
+class ightTurn : public Obstacle {
+
+};
+
+class Ramp : public Obstacle {
+
+};
+
 
 int main() {
 	std::cout << "Sleek Racer" << std::endl;
