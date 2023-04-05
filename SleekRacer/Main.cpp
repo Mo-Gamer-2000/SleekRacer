49eac2d5deb468abbc313d862d647fbb19d2649e
Moeez Abdul <MoeezRajput2000@Gmail.com>
2023-04-05 13:54:07 +0100

    SleekRacer class structured and implemented

--- SleekRacer/Main.cpp
+++ SleekRacer/Main.cpp
@@ -5,8 +5,31 @@
 #include <ctime>
 
 class SleekRacer {
+public:
+	SleekRacer(std::string name) : carName(name), distance(0), speed(0) {}
+	std::string getCarName() const
+	{
+		return carName;
+	}
+	int getDistance() const
+	{
+		return distance;
+	}
+	void setSpeed(int newSpeed)
+	{
+		speed = newSpeed;
+	}
+	void move()
+	{
+		distance += speed;
+	}
 
+	private;
+	std::string carName;
+	int distance;
+	int speed;
 };
+
 class PowerUp {
 
 };
@@ -19,7 +42,7 @@ class Obstacle {
 
 };
 
-class ightTurn : public Obstacle {
+class TightTurn : public Obstacle {
 
 };
 
